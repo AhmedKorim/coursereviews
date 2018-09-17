e53ffb87bb53ea41234d70a018c9ab7b924651f6
ahmedkorim <ahmedkorrim@gmail.com>
2018-09-17 18:35:07 +0300

    Initial commit

---
+++ src/Containers/App.js
@@ -0,0 +1,15 @@
+import Button from "@material-ui/core/Button/Button";
+import React, {Component} from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <Button>hi</Button>
+        </div>
+        );
+  }
+}
+
+export default App;
