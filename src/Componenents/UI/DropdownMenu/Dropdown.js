3a118b630556b742489d32c875d3ecad1283f378
ahmedkorim <ahmedkorrim@gmail.com>
2018-09-17 18:44:43 +0300

    form fields init

--- src/Componenents/UI/DropdownMenu/Dropdown.js
+++ src/Componenents/UI/DropdownMenu/Dropdown.js
@@ -1,10 +1,19 @@
+import Grid from "@material-ui/core/Grid/Grid";
 import React from 'react';
 
 class Dropdown extends React.Component {
+    state={}
     render() {
+        const {} = this.state;
         return (
             <div className="reviewCard">
-
+                <div className="container">
+                    <Grid container>
+                        <Grid item xs={12} md></Grid>
+                        <Grid item xs={12} md></Grid>
+                        <Grid item xs={12} md></Grid>
+                    </Grid>
+                </div>
             </div>
         )
     }
---
+++ src/Componenents/UI/FormInput/FormInpu.js
@@ -0,0 +1,62 @@
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
+import TextField from "@material-ui/core/TextField/TextField";
+import React, {Fragment} from 'react'
+
+const FormInput = props => {
+    const formcontroller = ({
+                                type = "text",
+                                label,
+                                multiline = false,
+                                value,
+                                id,
+                                multiline,
+                                placeHolder,
+                                changeHandler,
+                                options = []
+                            }) => {
+        switch (type) {
+            case 'select':
+                return (
+                    <TextField
+                        select
+                        name={id}
+                        id={id}
+                        label={label}
+                        className={classes.join(' ')}
+                        value={value}
+                        onChange={(event) => changeHandler(event, id)}
+                    >
+                        {options.map(option => <MenuItem key={option} value={option}>
+                            {option}
+                        </MenuItem>)}
+                    </TextField>)
+            default :
+                return (<TextField
+                    id={id}
+                    name={id}
+                    label={label}
+                    type={multiline ? null : type}
+                    multiline={multiline}
+                    classes=
+                        {multiline ? {
+                            root: 'textArea'
+                        } : null}
+                    rows={multiline ? '8' : null}
+                    placeholder={placeHolder}
+                    className={classes.join(' ')}
+                    value={value}
+                    onChange={(event) => changeHandler(event, id, other)}
+                    margin="normal"
+                />)
+
+        }
+    }
+
+
+    return (
+        <Fragment>
+            {formcontroller()}
+        </Fragment>
+    )
+}
+export default FormInput
